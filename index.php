8df0984bbf88aca650dd79b690284b0a5d7fdef9
THEO DEPELSEMACKER <depelsemackertheo@yahoo.fr>
2022-07-25 13:26:46 +0200

    Finish first labour

--- index.php
+++ index.php
@@ -1,4 +1,32 @@
 <?php
-
+require_once './src/Fighter.php';
 // First Labour : Heracles vs Nemean Lion
 // use your Figher class here
+
+$heracles = new Fighter('🧔 Heracles', 20, 6);
+
+$nemean_lion = new Fighter('🦁 Nemean Lion', 11, 13);
+
+function fightToDeath($fighter1, $fighter2)
+{
+    $round = 1;
+    while ($fighter1->life > 0 && $fighter2->life > 0)
+    {
+        echo "🕛 $round\n";
+        $fighter1->fight($fighter2);
+        if (!$fighter2->isAlive())
+        {
+            echo "🏆 $fighter1->name wins! (💙 $fighter1->life)\n";
+            break;
+        }
+        $fighter2->fight($fighter1);
+        if (!$fighter1->isAlive())
+        {
+            echo "🏆 $fighter2->name wins! (💙 $fighter2->life)\n";
+            break;
+        }
+        $round++;
+    }
+}
+
+fightToDeath($heracles, $nemean_lion);
--- src/Fighter.php
+++ src/Fighter.php
@@ -1,3 +1,35 @@
 <?php
 
-/* Fighter class definition */
+declare(strict_types = 1)
+;
+
+class Fighter
+{
+    const MAX_LIFE = 100;
+
+    public string $name;
+    public int $strength;
+    public int $dexterity;
+    public int $life;
+
+    function __construct(string $name, int $strength, int $dexterity)
+    {
+        $this->name = $name;
+        $this->strength = $strength;
+        $this->dexterity = $dexterity;
+        $this->life = self::MAX_LIFE;
+    }
+
+    public function fight(Fighter $opponent)
+    {
+        $damage = rand(1, $this->strength);
+        $damage = max(($damage - $opponent->dexterity), 1);
+        $opponent->life = max($opponent->life - $damage, 0);
+        echo "$this->name 🗡️  $opponent->name 💙 $opponent->name: $opponent->life\n";
+    }
+
+    public function isAlive(): bool
+    {
+        return $this->life > 0;
+    }
+}
